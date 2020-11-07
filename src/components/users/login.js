74da182850f53792cf2a4b47cbd8602645d96a02
Mug <brad.alexander913@gmail.com>
2020-11-07 09:48:34 -0800

    added state functionality to loginform

--- src/components/users/login.js
+++ src/components/users/login.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
 class LoginForm extends Component {
+state = {
+        text: '',
+    };
+
+handleChange = event => {
+    this.setState({
+        text: event.target.value
+    });
+};
+
     render() {
         return(
             <div>
                 <form>
                     <p>
                         <label>Login</label>
-                        <input type="username" />
+                        <input type="text" onChange={this.handleChange} value={this.state.text} />
                     </p>
                         <imput type="submit" />
                 </form>
