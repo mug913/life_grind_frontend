6f0b4a19dca547e49774774695c3ffe47e0bedbc
Mug <brad.alexander913@gmail.com>
2020-11-14 09:53:32 -0800

    fixed input error in loginForm

--- src/components/users/loginInput.js
+++ src/components/users/loginInput.js
@@ -4,11 +4,15 @@ import React, { Component } from 'react'
 class LoginInput extends Component {
 state = {
         username: '',
+        password: ''
     };
 
-handleChange = event => {
+handleChange = (field, event) => {
+    console.log(field)
+    console.log(event.target.value)
+    console.log(this.state)
     this.setState({
-        username: event.target.value
+        [field]: event.target.value
     });
 };
 
@@ -25,9 +29,9 @@ handleSubmit = event => {
             <div>
                 <form onSubmit={this.handleSubmit}>>
                     <label>Login</label>
-                        <input type="text" onChange={this.handleChange} value={this.state.username}/>
+                        <input type="text" onChange={this.handleChange.bind(this, 'username')} value={this.state.username}/>
                     <label>Password</label>
-                        <input type="password" onChange={this.handleChange} value={this.state.password}/>
+                        <input type="password" onChange={this.handleChange.bind(this, 'password')} value={this.state.password}/>
                         <input type="submit" />
                    <button >Register</button>
                 </form>
