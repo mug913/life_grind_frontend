7602de3609f45eab8dc9dbb38f755c18cff6a150
Mug <brad.alexander913@gmail.com>
2020-11-18 08:28:11 -0800

    reworked serializers to allow for goal record collection on user call

--- src/actions/goalActions.js
+++ src/actions/goalActions.js
@@ -3,7 +3,7 @@ import {addRecord}from './recordActions'
 
 const base_url = "http://127.0.0.1:9393/api/v1/"
 
-    export function getGoalEntries(user_id,goal_id) {
+    export function getGsoalEntries(user_id,goal_id) {
     return (dispatch) => {
       fetch(`${base_url}/users/${user_id}/goals/${goal_id}/goal_records`)
       .then(response => response.json())
--- src/components/records/record.js
+++ src/components/records/record.js
@@ -6,19 +6,12 @@ class Record extends Component {
   render() {
    
     return (
-      <div>
-        {record.name}
         <div>
-        {streak}{"\n"}
-        {level}{"\n"}
+        
         </div>
-        {this.optionDisplay(goal)}
-        </li>
-      </div>
     );
     }
-    return null
-  }
+    
 };
 
 export default Goal;
