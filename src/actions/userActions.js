3b31a5dcef98f96229d4c895f471e852dc30edb8
Mug <brad.alexander913@gmail.com>
2020-12-19 14:06:51 -0800

    rolled back userActions edit

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 
-const base_url = "https://immense-meadow-65005.herokuapp.com/api/v1/"
+const base_url = "http://127.0.0.1:9393/api/v1/"
 
 export function addUser(data) {
     return (dispatch) => {
@@ -20,17 +20,14 @@ export function addUser(data) {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(data)})
-        .then(response => {
-          if (!response.ok) {throw response}
-          return response.json()})
+        .then(response => response.json())
         .then(user => {dispatch(loginUser(user))})
-        }
       }
-    
+    }
     
   export function loginUser(id) {
     return (dispatch) => {
-      fetch(`${base_url}/users/${id}`)
+      fetch(`${base_url}users/${id}`)
       .then(response => response.json())
       .then(user_data => {
         dispatch({ type: 'LOGIN_USER', payload: user_data })
