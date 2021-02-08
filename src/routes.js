2b2a0fbe089bd4eaf40b861642214588da300a8b
Mug <brad.alexander913@gmail.com>
2021-02-07 19:46:37 -0800

    redid login auth and rough edit to make redirect on logout. needs cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route, BrowserRouter as Router, Switch, Redirect} from "react-router-dom";
-
+import {connect} from 'react-redux'
+import { AppContext } from "./libs/contextLib"
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Home from "./pages/home/home";
 import Error from "./pages/error/error";
-const Routes = (props) => (
+
+
+function Routes(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  
+  return( <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated}} >
   <Router {...props}>
     <Switch>
       <Route path="/login">
@@ -15,7 +21,7 @@ const Routes = (props) => (
         <Register />
       </Route>
       <Route path="/home">
-        <Home />
+        {props.user_id === "" ? <Login/> : <Home />}
       </Route>
       <Route exact path="/">
         <Redirect to="/login" />
@@ -24,6 +30,16 @@ const Routes = (props) => (
         <Error />
       </Route>
     </Switch>
-  </Router>
-);
-export default Routes;
+   </Router>
+   
+  </AppContext.Provider>)
+};
+
+
+const mapStateToProps = (state) => {
+  return {
+    user_id: state.usersReducer.user_id
+ }
+}
+
+export default connect(mapStateToProps)(Routes);
